92da7045598ce4f40aff1b050d24833898028bd8
Lucianne <lu.pedroti@gmail.com>
2019-03-18 17:23:04 -0300

    Criar página sobre o projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+// let clicar = document.querySelector("#clicar");
+// let pagina = document.querySelector("#corpo");
+
+// function trocarCor () {
+//     pagina.style.backgroundColor = "green";
+// }
+
+// botao.onclick = trocarCor;
+
